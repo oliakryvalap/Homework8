e79c94395e4da65c0c2c8ffcae861bdcdc5fc7f5
Olga <olia_tarasenko@inbox.ru>
2023-03-10 00:28:38 +0300

    Задача1

---
+++ Task 5/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//Напишите программу, которая заполнит спирально массив 4 на 4.
+//Например, на выходе получается вот такой массив:
+//01 02 03 04
+//12 13 14 05
+int[,] array = new int[4, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    Console.WriteLine();
+   for (int j = 0; j < array.GetLength(1); j++) 
+   {
+   array[i, j] = new Random().Next(2, 7);
+   Console.Write(array[i, j] + " ");
+}
+}
